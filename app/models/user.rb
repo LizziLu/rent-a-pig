ad9e14879705865dd391ab375a745661e0fc9897
Marie Gaar <marie_gaar@gmx.at>
2021-05-24 17:34:34 +0200

    associations and validations for pig and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-  has_secure_password
+  has_many :pigs, dependent: :destroy
+  has_many :bookings
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, email: true, uniqueness: true
+  validates :password, presence: true
 end
