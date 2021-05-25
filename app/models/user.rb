5d933fd5b4b9c7966a71379805d541ca026ea5ad
Lisa Ziegler <zieglerlisa@gmx.de>
2021-05-25 11:06:14 +0200

    user model with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
-  has_many :pigs, dependent: :destroy
-  has_many :bookings
-
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true, email: true, uniqueness: true
-  validates :password, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
