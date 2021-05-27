600a236ae5cd91b093ca7e949bb8685c0bcdbae8
Tim Moehrke <tim_moehrke@gmx.de>
2021-05-27 17:29:13 +0200

    fixed the seeds and also double fixed the map on index

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,9 @@
 require 'faker'
 require "open-uri"
 
+User.destroy_all
+Pig.destroy_all
+
 puts 'Creating 5 fake users'
 5.times do
   user = User.new(
@@ -29,6 +32,7 @@ puts "creating 25 fake pigs"
   name: "Luis",
   user: User.last,
   profession: "Tax Auditor",
+  address: "Am Lustgarten 1, Berlin",
   weight: rand(25..500),
   age: rand(2..100),
   price: rand(1..100),
@@ -42,6 +46,7 @@ pig = Pig.new(
 name: "Frances",
 user: User.last,
 profession: "Weapons Dealer",
+address: "Am Kupfergraben 1, Berlin",
 weight: rand(25..500),
 age: rand(2..100),
 price: rand(1..100),
@@ -55,12 +60,13 @@ pig = Pig.new(
 name: "Barbara",
 user: User.last,
 profession: "Un-Diplomat",
+address: "Pfarrgasse 2, Böblingen",
 weight: rand(25..500),
 age: rand(2..100),
 price: rand(1..100),
 bio: "Barbara is works as a UN-Diplomat in the central area of Africa. She has seen a lot and sometimes wishes to escape her stressful day to day job and become a gardener in the South of France.",
 )
-file = URI.open("https://images.unsplash.com/photo-1598493253616-1f2bc5d63991?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=789&q=80")
+file = URI.open("https://images.unsplash.com/photo-1598493253616-1f2bc5d63991?fit=crop&w=800&q=80")
 pig.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
 pig.save!
 
@@ -69,12 +75,13 @@ pig = Pig.new(
 name: "Gonzales",
 user: User.last,
 profession: "Woodworker",
+address: "Turmhof 8, Wuppertal",
 weight: rand(25..500),
 age: rand(2..100),
 price: rand(1..100),
 bio: "Gonzales makes his own furniture. He is skilled in pretty much everything. He is truly a versatile pig. If you do not really know why you need a pig but are sure that you do need a pig. Gonzales is your pick!.",
 )
-file = URI.open("https://images.unsplash.com/photo-1587007451503-4a5aa58abfc5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80")
+file = URI.open("https://images.unsplash.com/photo-1587007451503-4a5aa58abfc5")
 pig.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
 pig.save!
 
@@ -82,12 +89,13 @@ pig = Pig.new(
 name: "Candice",
 user: User.last,
 profession: "Free Spirit",
+address: "Universitätsstraße 100, Köln",
 weight: rand(25..500),
 age: rand(2..100),
 price: rand(1..100),
 bio: "Candice makes people happy. She oinks a lot and likes to eat. A simple pig for people with complicated lives. Time with Candice never seems wasted.",
 )
-file = URI.open("https://images.unsplash.com/photo-1533586473300-f66da55c9cf7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80")
+file = URI.open("https://images.unsplash.com/photo-1533586473300-f66da55c9cf7?fit=crop&w=800&q=80")
 pig.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
 pig.save!
 
@@ -95,12 +103,13 @@ pig = Pig.new(
 name: "Juliette",
 user: User.last,
 profession: "Sales Person",
+address: "Roncalliplatz 4, Köln",
 weight: rand(25..500),
 age: rand(2..100),
 price: rand(1..100),
 bio: "Juliette is awful. I only offer her here to get some time away from her. Seriously she sucks! But by booking her you would do the good deed of giving me some free space I desperately need.",
 )
-file = URI.open("https://images.unsplash.com/photo-1546981969-f0c74fe14902?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80")
+file = URI.open("https://images.unsplash.com/photo-1546981969-f0c74fe14902?fit=crop&w=800&q=80")
 pig.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
 pig.save!
 
@@ -108,12 +117,13 @@ pig = Pig.new(
 name: "Carol",
 user: User.last,
 profession: "F-22 Pilot",
+address: "Schlachthofstraße 2A, Stuttgart",
 weight: rand(25..500),
 age: rand(2..100),
 price: rand(1..100),
 bio: "Carol always knew she wanted to be a fighter jet pilot. Starting as a little suckling she threw herself of barn roofs and throwing darts at her little brothers and sisters. Some years later she is now a decorated general in the army of Costa Rica.",
 )
-file = URI.open("https://images.unsplash.com/photo-1543360308-232162ffd6e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80")
+file = URI.open("https://images.unsplash.com/photo-1543360308-232162ffd6e2?fit=crop&w=800&q=80")
 pig.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
 pig.save!
 
@@ -121,12 +131,13 @@ pig = Pig.new(
   name: "Richard",
   user: User.last,
   profession: "Stoner",
+  address: "Salzstadelgasse 10, Ulm",
   weight: rand(25..500),
   age: rand(2..100),
   price: rand(1..100),
   bio: "Richard likes Bob Ross and the occasional joint. He is a wanted fugitive in pretty much all of South East Asia. He makes Kickass French Toast and is a relaxed pig to spend time with.",
   )
-  file = URI.open("https://images.unsplash.com/photo-1516467508483-a7212febe31a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80")
+  file = URI.open("https://images.unsplash.com/photo-1516467508483-a7212febe31a?fit=crop&w=800&q=80")
   pig.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
   pig.save!
 
@@ -134,12 +145,13 @@ pig = Pig.new(
 name: "Piggy",
 user: User.last,
 profession: "Entrepeneur",
+address: "Klosterwall 15, Hamburg",
 weight: rand(25..500),
 age: rand(2..100),
 price: rand(1..100),
 bio: "Piggy likes frogs. I do not know why. I do not even know how they communicate. It´s weird. But it´s her thing. So if you want a really open minded pig without any judgments. Go for it.",
 )
-file = URI.open("https://lh3.googleusercontent.com/proxy/VvzdjynuEoYZJDsYHMoxiq0IlTx5GCblcWrWqhJ_mEkSaGfQ97skwgPDmKljpHWqmxRD2zvIo4-XhZyNRR9HnHxKgZD6jzfHquo")
+file = URI.open("https://ais.badische-zeitung.de/piece/06/4e/cb/4d/105827149-h-720.jpg")
 pig.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
 pig.save!
 
@@ -147,6 +159,7 @@ pig = Pig.new(
 name: "Trixie",
 user: User.last,
 profession: "Ballerina",
+address: "Kaiser-Wilhelm-Str. 2, Saarlouis",
 weight: rand(25..500),
 age: rand(2..100),
 price: rand(1..100),
@@ -160,12 +173,13 @@ pig = Pig.new(
 name: "Klaus",
 user: User.last,
 profession: "Movie Star",
+address: "Bodestraße 1, Berlin",
 weight: rand(25..500),
 age: rand(2..100),
 price: rand(1..100),
 bio: "Starting at a young age, Klaus never failed to impress to world with his ability to adapt to any and every role. He has starred in multiple award winning movies and is nominated for the Oscars this year for his latest performance, so keep your pen sharp for your autograph!",
 )
-file = URI.open("https://images.unsplash.com/photo-1550825570-1959166c6e66?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=890&q=80")
+file = URI.open("https://images.unsplash.com/photo-1550825570-1959166c6e66?fit=crop&w=800&q=80")
 pig.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
 pig.save!
 
@@ -173,6 +187,7 @@ pig = Pig.new(
 name: "Erika",
 user: User.last,
 profession: "Unemployed",
+address: "Museumstraße 23, Hamburg",
 weight: rand(25..500),
 age: rand(2..100),
 price: rand(1..100),
@@ -186,12 +201,13 @@ pig = Pig.new(
 name: "Leo",
 user: User.last,
 profession: "Bartender",
+address: "Lerchenfeldstraße 3, München",
 weight: rand(25..500),
 age: rand(2..100),
 price: rand(1..100),
-bio: " If you thought you’ve had good cocktails before, think again! Leo is the greatest of the great and will leave your heart filled with joy and your head with bubbles as he mixes heaven on earth for you.",
+bio: "If you thought you’ve had good cocktails before, think again! Leo is the greatest of the great and will leave your heart filled with joy and your head with bubbles as he mixes heaven on earth for you.",
 )
-file = URI.open("https://images.unsplash.com/photo-1591175660774-2a59438ceaf5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=747&q=80")
+file = URI.open("https://images.unsplash.com/photo-1591175660774-2a59438ceaf5?fit=crop&w=800&q=80")
 pig.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
 pig.save!
 
@@ -200,6 +216,7 @@ pig = Pig.new(
 name: "Esther",
 user: User.last,
 profession: "Rockstar",
+address: "Kurfürstendamm 30, Berlin",
 weight: rand(25..500),
 age: rand(2..100),
 price: rand(1..100),
