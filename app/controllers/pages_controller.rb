a5dbdb9c3a4663f208a7903e0bb2150242430d61
Lisa Ziegler <zieglerlisa@gmx.de>
2021-05-27 12:26:17 +0200

    controller and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
   end
+
+  def dashboard
+
+    @offered_pigs = Pig.where(user: current_user)
+    @bookings = current_user.bookings
+  end
 end
